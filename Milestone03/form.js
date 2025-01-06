6ea28a5734fc919c7fbc87e3047c0f86fd07f51b
Kulsoom Imran <kulsoomimran1901@gmail.com>
2025-01-06 18:40:42 +0500

    update the code

--- Milestone03/form.js
+++ Milestone03/form.js
@@ -1,61 +1,34 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+var _a;
 //Show input feilds on the clicking on start button
-const startButton = document.getElementById("startButton");
-const container = document.getElementById("container");
-const openButton = document.getElementById("openButton");
+var startButton = document.getElementById("startButton");
+var container = document.getElementById("container");
+var openButton = document.getElementById("openButton");
 if (startButton && container) {
-    startButton.addEventListener("click", () => {
+    startButton.addEventListener("click", function () {
         openButton.style.display = "none";
         container.style.display = "block";
     });
 }
 // Generate the resume content on form submission
-document.getElementById('resume-form')?.addEventListener('submit', function (event) {
+(_a = document.getElementById('resume-form')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
     event.preventDefault();
     // Capture user inputs
-    const name = document.getElementById('name').value;
-    const obj = document.getElementById('obj').value;
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const profilePictureInput = document.getElementById('profilePicture');
-    const profilePictureFile = profilePictureInput.files ? profilePictureInput.files[0] : null;
-    const profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
-    const educations = Array.from(document.getElementsByClassName('education')).map(input => input.value);
-    const certifications = Array.from(document.getElementsByClassName('certification')).map(input => input.value);
-    const works = Array.from(document.getElementsByClassName('work')).map(input => input.value);
-    const skills = Array.from(document.getElementsByClassName('skill')).map(input => input.value);
-    const reference = document.getElementById('reference').value;
+    var name = document.getElementById('name').value;
+    var obj = document.getElementById('obj').value;
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var profilePictureInput = document.getElementById('profilePicture');
+    var profilePictureFile = profilePictureInput.files ? profilePictureInput.files[0] : null;
+    var profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
+    var educations = Array.from(document.getElementsByClassName('education')).map(function (input) { return input.value; });
+    var certifications = Array.from(document.getElementsByClassName('certification')).map(function (input) { return input.value; });
+    var works = Array.from(document.getElementsByClassName('work')).map(function (input) { return input.value; });
+    var skills = Array.from(document.getElementsByClassName('skill')).map(function (input) { return input.value; });
+    var reference = document.getElementById('reference').value;
     // the resume section with the gathered information
-    const generatedResume = document.getElementById('generated-resume');
-    generatedResume.innerHTML = `
-        <div class="resume-content">
-            <h2>${name}</h2>
-            <p><strong>Objective:</strong> ${obj}</p>
-            <h3>Personal Information</h3>
-            ${profilePictureURL ? `<img src="${profilePictureURL}" alt="Profile Picture" class="profilePicture">` : ''}
-            <p><strong>First Name:</strong> ${firstName}</p>
-            <p><strong>Last Name:</strong> ${lastName}</p>
-            <p><strong>Email:</strong> ${email}</p>
-            <p><strong>Phone:</strong> ${phone}</p>
-           
-            <h3>Education</h3>
-            <ul>${educations}</ul>
-            
-            <h3>Certification</h3>
-            <ul>${certifications}</ul>
-
-            <h3>Work Experience</h3>
-            <ul>${works}</ul>
-             
-            <h3>Skills</h3>
-            <ul>${skills}</ul>
-
-            <p><strong>Reference:</strong> ${reference}</p>
-
-            </div>
-    `;
+    var generatedResume = document.getElementById('generated-resume');
+    generatedResume.innerHTML = "\n        <div class=\"resume-content\">\n            <h2>".concat(name, "</h2>\n            <p><strong>Objective:</strong> ").concat(obj, "</p>\n            <h3>Personal Information</h3>\n            ").concat(profilePictureURL ? "<img src=\"".concat(profilePictureURL, "\" alt=\"Profile Picture\" class=\"profilePicture\">") : '', "\n            <p><strong>First Name:</strong> ").concat(firstName, "</p>\n            <p><strong>Last Name:</strong> ").concat(lastName, "</p>\n            <p><strong>Email:</strong> ").concat(email, "</p>\n            <p><strong>Phone:</strong> ").concat(phone, "</p>\n           \n            <h3>Education</h3>\n            <ul>").concat(educations, "</ul>\n            \n            <h3>Certification</h3>\n            <ul>").concat(certifications, "</ul>\n\n            <h3>Work Experience</h3>\n            <ul>").concat(works, "</ul>\n             \n            <h3>Skills</h3>\n            <ul>").concat(skills, "</ul>\n\n            <p><strong>Reference:</strong> ").concat(reference, "</p>\n\n            </div>\n    ");
     document.getElementById('resume').style.display = 'block';
 });
--- Milestone04/main.js
+++ Milestone04/main.js
@@ -1,116 +1,74 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+var _a;
 // Function to add an additional education field
 function addEducationField() {
-    const educationSection = document.getElementById('education-section');
-    const newEducationEntry = document.createElement('div');
+    var educationSection = document.getElementById('education-section');
+    var newEducationEntry = document.createElement('div');
     newEducationEntry.className = 'education-entry';
-    newEducationEntry.innerHTML = `
-        <label>Degree and School:</label>
-        <input type="text" class="education" name="education" required>
-    `;
+    newEducationEntry.innerHTML = "\n        <label>Degree and School:</label>\n        <input type=\"text\" class=\"education\" name=\"education\" required>\n    ";
     educationSection.appendChild(newEducationEntry);
 }
 // Function to add an additional language field
 function addLanguageField() {
-    const languageSection = document.getElementById('language-section');
-    const newLanguageEntry = document.createElement('div');
+    var languageSection = document.getElementById('language-section');
+    var newLanguageEntry = document.createElement('div');
     newLanguageEntry.className = 'language-entry';
-    newLanguageEntry.innerHTML = `
-        <label>Language:</label>
-        <input type="text" class="language" name="language" required>
-    `;
+    newLanguageEntry.innerHTML = "\n        <label>Language:</label>\n        <input type=\"text\" class=\"language\" name=\"language\" required>\n    ";
     languageSection.appendChild(newLanguageEntry);
 }
 // Function to add an additional certification field
 function addCertificationField() {
-    const CertificationSection = document.getElementById('certification-section');
-    const newCertificationEntry = document.createElement('div');
+    var CertificationSection = document.getElementById('certification-section');
+    var newCertificationEntry = document.createElement('div');
     newCertificationEntry.className = 'certification-entry';
-    newCertificationEntry.innerHTML = `
-        <label>Degree and School:</label>
-        <input type="text" class="certification" name="certification" required>
-    `;
+    newCertificationEntry.innerHTML = "\n        <label>Degree and School:</label>\n        <input type=\"text\" class=\"certification\" name=\"certification\" required>\n    ";
     CertificationSection.appendChild(newCertificationEntry);
 }
 // Function to add an additional work field
 function addWorkField() {
-    const workSection = document.getElementById('work-section');
-    const newWorkEntry = document.createElement('div');
+    var workSection = document.getElementById('work-section');
+    var newWorkEntry = document.createElement('div');
     newWorkEntry.className = 'work-entry';
-    newWorkEntry.innerHTML = `
-        <label>Position and Company:</label>
-        <input type="text" class="work" name="work" required>
-    `;
+    newWorkEntry.innerHTML = "\n        <label>Position and Company:</label>\n        <input type=\"text\" class=\"work\" name=\"work\" required>\n    ";
     workSection.appendChild(newWorkEntry);
 }
 // Function to add an additional skill field
 function addSkillField() {
-    const skillsSection = document.getElementById('skills-section');
-    const newSkillEntry = document.createElement('div');
+    var skillsSection = document.getElementById('skills-section');
+    var newSkillEntry = document.createElement('div');
     newSkillEntry.className = 'skills-entry';
-    newSkillEntry.innerHTML = `
-        <label>Skill:</label>
-        <input type="text" class="skill" name="skills" required>
-    `;
+    newSkillEntry.innerHTML = "\n        <label>Skill:</label>\n        <input type=\"text\" class=\"skill\" name=\"skills\" required>\n    ";
     skillsSection.appendChild(newSkillEntry);
 }
 //Show input feilds on the clicking on start button
-const startButton = document.getElementById("startButton");
-const container = document.getElementById("container");
-const openButton = document.getElementById("openButton");
+var startButton = document.getElementById("startButton");
+var container = document.getElementById("container");
+var openButton = document.getElementById("openButton");
 if (startButton && container) {
-    startButton.addEventListener("click", () => {
+    startButton.addEventListener("click", function () {
         openButton.style.display = "none";
         container.style.display = "block";
     });
 }
 // Generate the resume content on form submission
-document.getElementById('resume-form')?.addEventListener('submit', function (event) {
+(_a = document.getElementById('resume-form')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
     event.preventDefault();
     // Capture user inputs
-    const name = document.getElementById('name').value;
-    const obj = document.getElementById('obj').value;
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const profilePictureInput = document.getElementById('profilePicture');
-    const profilePictureFile = profilePictureInput.files ? profilePictureInput.files[0] : null;
-    const profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
-    const educations = Array.from(document.getElementsByClassName('education')).map(input => input.value);
-    const certifications = Array.from(document.getElementsByClassName('certification')).map(input => input.value);
-    const works = Array.from(document.getElementsByClassName('work')).map(input => input.value);
-    const skills = Array.from(document.getElementsByClassName('skill')).map(input => input.value);
-    const reference = document.getElementById('reference').value;
+    var name = document.getElementById('name').value;
+    var obj = document.getElementById('obj').value;
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var profilePictureInput = document.getElementById('profilePicture');
+    var profilePictureFile = profilePictureInput.files ? profilePictureInput.files[0] : null;
+    var profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
+    var educations = Array.from(document.getElementsByClassName('education')).map(function (input) { return input.value; });
+    var certifications = Array.from(document.getElementsByClassName('certification')).map(function (input) { return input.value; });
+    var works = Array.from(document.getElementsByClassName('work')).map(function (input) { return input.value; });
+    var skills = Array.from(document.getElementsByClassName('skill')).map(function (input) { return input.value; });
+    var reference = document.getElementById('reference').value;
     // the resume section with the gathered information
-    const generatedResume = document.getElementById('generated-resume');
-    generatedResume.innerHTML = `
-        <div class="resume-content">
-            <h2>${name}</h2>
-            <p><strong>Objective:</strong> ${obj}</p>
-            <h3>Personal Information</h3>
-            ${profilePictureURL ? `<img src="${profilePictureURL}" alt="Profile Picture" class="profilePicture">` : ''}
-            <p><strong>First Name:</strong> ${firstName}</p>
-            <p><strong>Last Name:</strong> ${lastName}</p>
-            <p><strong>Email:</strong> ${email}</p>
-            <p><strong>Phone:</strong> ${phone}</p>
-           
-            <h3>Education</h3>
-            <ul>${educations.map(edu => `<li>${edu}</li>`).join('')}</ul>
-            
-            <h3>Certification</h3>
-            <ul>${certifications.map(certi => `<li>${certi}</li>`).join('')}</ul>
-
-            <h3>Work Experience</h3>
-            <ul>${works.map(work => `<li>${work}</li>`).join('')}</ul>
-             
-            <h3>Skills</h3>
-            <ul>${skills.map(skill => `<li>${skill}</li>`).join('')}</ul>
-
-            <p><strong>Reference:</strong> ${reference}</p>
-
-            </div>
-    `;
+    var generatedResume = document.getElementById('generated-resume');
+    generatedResume.innerHTML = "\n        <div class=\"resume-content\">\n            <h2>".concat(name, "</h2>\n            <p><strong>Objective:</strong> ").concat(obj, "</p>\n            <h3>Personal Information</h3>\n            ").concat(profilePictureURL ? "<img src=\"".concat(profilePictureURL, "\" alt=\"Profile Picture\" class=\"profilePicture\">") : '', "\n            <p><strong>First Name:</strong> ").concat(firstName, "</p>\n            <p><strong>Last Name:</strong> ").concat(lastName, "</p>\n            <p><strong>Email:</strong> ").concat(email, "</p>\n            <p><strong>Phone:</strong> ").concat(phone, "</p>\n           \n            <h3>Education</h3>\n            <ul>").concat(educations.map(function (edu) { return "<li>".concat(edu, "</li>"); }).join(''), "</ul>\n            \n            <h3>Certification</h3>\n            <ul>").concat(certifications.map(function (certi) { return "<li>".concat(certi, "</li>"); }).join(''), "</ul>\n\n            <h3>Work Experience</h3>\n            <ul>").concat(works.map(function (work) { return "<li>".concat(work, "</li>"); }).join(''), "</ul>\n             \n            <h3>Skills</h3>\n            <ul>").concat(skills.map(function (skill) { return "<li>".concat(skill, "</li>"); }).join(''), "</ul>\n\n            <p><strong>Reference:</strong> ").concat(reference, "</p>\n\n            </div>\n    ");
     document.getElementById('resume').style.display = 'block';
 });
